88892b7b098f93166d3046826afbc0a09be001e7
hamishhuggard <hamishhuggard@gmail.com>
2022-11-16 19:02:51 +1300

    Make draggable links with images

--- main.js
+++ main.js
@@ -1,108 +1,44 @@
-
-// Copyright 2021 Observable, Inc.
-// Released under the ISC license.
-// https://observablehq.com/@d3/scatterplot
-function Scatterplot(data, {
-  x = ([x]) => x, // given d in data, returns the (quantitative) x-value
-  y = ([, y]) => y, // given d in data, returns the (quantitative) y-value
-  r = 3, // (fixed) radius of dots, in pixels
-  title, // given d in data, returns the title
-  marginTop = 20, // top margin, in pixels
-  marginRight = 30, // right margin, in pixels
-  marginBottom = 30, // bottom margin, in pixels
-  marginLeft = 40, // left margin, in pixels
-  inset = r * 2, // inset the default range, in pixels
-  insetTop = inset, // inset the default y-range
-  insetRight = inset, // inset the default x-range
-  insetBottom = inset, // inset the default y-range
-  insetLeft = inset, // inset the default x-range
-  width = 640, // outer width, in pixels
-  height = 400, // outer height, in pixels
-  xDomain, // [xmin, xmax]
-  xRange = [marginLeft + insetLeft, width - marginRight - insetRight], // [left, right]
-  yDomain, // [ymin, ymax]
-  yRange = [height - marginBottom - insetBottom, marginTop + insetTop], // [bottom, top]
-  xLabel, // a label for the x-axis
-  yLabel, // a label for the y-axis
-  xFormat, // a format specifier string for the x-axis
-  yFormat, // a format specifier string for the y-axis
-  fill = "none", // fill color for dots
-  stroke = "currentColor", // stroke color for the dots
-  strokeWidth = 1.5, // stroke width for dots
-  halo = "#fff", // color of label halo 
-  haloWidth = 3 // padding around the labels
-} = {}) {
-  // Compute values.
-  const X = d3.map(data, x);
-  const Y = d3.map(data, y);
-  const T = title == null ? null : d3.map(data, title);
-  const I = d3.range(X.length).filter(i => !isNaN(X[i]) && !isNaN(Y[i]));
-
-  // Compute default domains.
-  if (xDomain === undefined) xDomain = d3.extent(X);
-  if (yDomain === undefined) yDomain = d3.extent(Y);
-
-  // Construct scales and axes.
-  const xScale = d3.scaleLinear(xDomain, xRange);
-  const yScale = d3.scaleLinear(yDomain, yRange);
-  const xAxis = d3.axisBottom(xScale).ticks(width / 80, xFormat);
-  const yAxis = d3.axisLeft(yScale).ticks(height / 50, yFormat);
-
-  const viz = d3.select("#my_dataviz")
-
-  const svg = viz.append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("viewBox", [0, 0, width, height])
-      .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
-
-  if (T) svg.append("g")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 10)
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-    .selectAll("text")
-    .data(I)
-    .join("text")
-      .attr("dx", 7)
-      .attr("dy", "0.35em")
-      .attr("x", i => xScale(X[i]))
-      .attr("y", i => yScale(Y[i]))
-      .text(i => T[i])
-      .call(text => text.clone(true))
-      .attr("fill", "none")
-      .attr("stroke", halo)
-      .attr("stroke-width", haloWidth)
-
-  svg.append("g")
-      .attr("fill", fill)
-      .attr("stroke", stroke)
-      .attr("stroke-width", strokeWidth)
-    .selectAll("circle")
-    .data(I)
-    .join("circle")
-      .attr("cx", i => xScale(X[i]))
-      .attr("cy", i => yScale(Y[i]))
-      .attr("r", r);
-
-}
-
 let sheetUrl = 'content.csv'
 sheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRRUv6PC5hC4-VXzQy75DBeywJaiQjU7MPGOoZBat9iJCmQo9Pf0nc2nvAFDfRJmP06WHJEls4RgUw6/pub?gid=1535062603&single=true&output=csv'
 
 d3.csv(sheetUrl).then(function(data) {
 
-console.table(data);
+    delete data['columns'];
+    console.table(data);
 
-chart = Scatterplot(data, {
-  x: d => parseFloat( d['x'] ),
-  y: d => parseFloat( d['y'] ),
-  title: d => d['Label'],
-  xLabel: "x →",
-  yLabel: "↑ y",
-  stroke: "steelblue",
-  width: 600,
-  height: 600
-})
+    data.map(function(d) {d.x = parseFloat(d.x); d.y = parseFloat(d.y)});
+
+    const X = d3.map(data, d=>d.x);
+    const Y = d3.map(data, d=>d.y);
+    const xDomain = d3.extent(X);
+    const yDomain = d3.extent(Y);
+    const xScale = d3.scaleLinear(xDomain, [10,90]);
+    const yScale = d3.scaleLinear(yDomain, [10,90]);
+
+    console.log(xScale(0), yScale(0), xDomain, yDomain);
+
+    const divs = d3.select("#map2")
+        .selectAll('div')
+        .data(data)
+        .enter()
+        .append('div')
+        .attr('style', d => 
+            `position: absolute; left: ${xScale(d.x)}%; top: ${yScale(d.y)}%; transform: translateX(-50%);`
+        )
+        .attr('class', 'draggable')
+    divs
+            .append('img')
+            .attr('src', d => d.logo)
+            .attr('width', '30px')
+    divs
+            .append('br')
+    divs
+            .append('a')
+            .attr('href', d => d.Link)
+            .attr('title', d => d.Hovertext)
+            .html(d => d.Label);
+
+
+    $( ".draggable" ).draggable();
 
 });
